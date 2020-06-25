0aefc22ac20ea8e5aa8eafe871cd2d9ee7cadd97
otetLopez <otet.tud@gmail.com>
2020-06-25 15:07:25 -0400

    Add pswd, dload support

--- scripts/encrypt.js
+++ scripts/encrypt.js
@@ -2,23 +2,24 @@
 import { File }  from './file.js';
 
 // File Constants
-const SUCCESS = true;
+const SUCCESS = 0;
 const ERROR = -1;
+const INFO_LOG = "INFO_DEBUG";
 
 export function encryptFile(file) {
     if(!file.isEncrypted) {
-        console.log("INFO_DEBUG: Starting Encryption on file " + file.content + " with pwd " + file.pwd);
+        console.log(INFO_LOG + "Starting Encryption on file " + file.content + " with pwd " + file.pwd);
         var textToEncrypt = file.content;
         var password = file.pwd;
-        var encyrpted = "";
+        var encyrpted = password + "xyzcryptoxyz"; //So we can decode password
         var i;
         for(i=0; i<textToEncrypt.length; i++) {
             encyrpted += password + textToEncrypt[i];
         }
-        console.log("INFO_DEBUG: Encrypted Data is " + encyrpted);
-        return SUCCESS;
+        //console.log("INFO_DEBUG: Encrypted Data is " + encyrpted);
+        return encyrpted;
     } else {
-        console.log("INFO_DEBUG: Inputted file is already encyrpted");
+        console.log(INFO_LOG + " Inputted file is already encyrpted");
         return ERROR;
     }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -5,17 +5,21 @@ import { encryptFile } from './encrypt.js';
 // Constants
 const ERROR = -1;
 const SUCCESS = 0;
+const INFO_LOG = "INFO_DEBUG: ";
 
-console.log("INFO_DEBUG: Starting main javascript");
+console.log(INFO_LOG + "Starting main javascript");
 
 /*** This is the main functionality and shall be triggered by events from the site */
+
 var textFromFileLoaded = "";
+var encrypted = "";
+var decrypted = "";
 var custom_pwd = "";
 
 $(document).ready(function () {
     $("button").click(function() { 
         var btn_id = $(this).attr('id');
-        console.log("INFO_DEBUG: You Pressed " + btn_id);
+        console.log(INFO_LOG + "You Pressed " + btn_id);
         if(btn_id === "btn_load") {
             textFromFileLoaded= "" ;
             var fileToLoad = document.getElementById("fileToLoad").files[0];
@@ -28,7 +32,7 @@ $(document).ready(function () {
                 //     textFromFileLoaded += lines[line];
                 // }
                 textFromFileLoaded = this.result;
-                console.log("INFO_DEBUG: " + textFromFileLoaded + "\n ******** End of Text ********");
+                console.log(INFO_LOG + textFromFileLoaded + "\n ******** End of Text ********");
                 // Check if file opened is not empty
                 if(textFromFileLoaded.length > 0) {
                     var fileToEncrypt = new File(textFromFileLoaded, getUserPassword(), false);
@@ -37,19 +41,45 @@ $(document).ready(function () {
             };
             reader.readAsText(fileToLoad);
         }
+        else if(btn_id === "btn_dload_e") {
+            // Download encrypted file
+            createFileForDload(encrypted);
+        }
+        else if(btn_id === "btn_dload_d") {
+            // Download decrypted file
+            createFileForDload(decrypted);
+        }
     }); 
 }); 
 
 function processFile(fileToEncrypt) {
     // Should be implementing a PROMISE here
-    if(encryptFile(fileToEncrypt) === ERROR) {
+    encrypted = "";
+    encrypted = encryptFile(fileToEncrypt);
+    if(encrypted === ERROR) {
         console.error("ENCRYPTION ERROR");
-    } 
+        
+    }  else {
+        console.log(INFO_LOG + "Encrypted Data is " + encrypted);
+
+    }
+}
+
+function createFileForDload(content) {
+    if(!content.length > 0) {
+        // No File To Save
+        alert("ERROR: No file to save.  Please upload file to encrypt or decrypt");
+        console.log(INFO_LOG + "No file to save.")
+    } else {
+        window.open("data:application/txt," + encodeURIComponent(content), "_self");
+    }
 }
 
 function getUserPassword() {
     // Should retrieve password inputted by user
     // Temporarily returning defaults
-    return "default";
+    var code = document.getElementById('pwd').value;
+    console.log(INFO_LOG + "This is the Ecryption Code >> " + code);
+    return (code.length > 0) ? code : "default";
 }
 
