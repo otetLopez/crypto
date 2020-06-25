91a95048b29cbe39f3d4bba03fb167c051e4fab9
otetLopez <otet.tud@gmail.com>
2020-06-24 22:03:19 -0400

    Modifying file object, adding attribute password

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,35 @@
+// import { encryptFile } from './encrypt';
 
-function inputFile(fname, isEncrypted) {
-    // this.fileName = fileName;
-    var fname = fname;
+// File Constants
+const SUCCESS = true;
+const ERROR = false;
+
+/**
+ * Inputfile object is created whenever there is a fie being uploaded by the user
+ * @param {string} fname 
+ * @param {string} password
+ * @param {boolean} isEncrypted 
+ */
+function InputFile(fname, password, isEncrypted) {
+    var filename = fname;
+    var pwd = password;
     var isEncrypted = isEncrypted;
 
     Object.defineProperties(this, {
         "fname": {
             get: function () {
-                return fname;
+                return filename;
             },
             set: function (value) {
-                fname = value;
+                filename = value;
+            }
+        },
+        "pwd": {
+            get: function () {
+                return pwd;
+            },
+            set: function (value) {
+                pwd = value;
             }
         },
         "isEncrypted": {
@@ -24,15 +43,29 @@ function inputFile(fname, isEncrypted) {
     });
 }
 
+function encryptFile(file) {
+    if(!file.isEncrypted) {
+        console.log("INFO_DEBUG: Starting Encryption on file " + file.fname);
+        return SUCCESS;
+    } else {
+        console.log("INFO_DEBUG: Inputted file is already encyrpted");
+        return ERROR;
+    }
+  
+}
+
+
 /*** Sample Debug Files Start ***/
-var file1 = new inputFile("dannie", true);
-console.log(file1.fname + " is " + (file1.isEncrypted ? "Encrypted." : "Decrypted"));
+var file1 = new InputFile("dannie", "password", true);
+console.log("INFO_DEBUG: " + file1.fname + " is " + (file1.isEncrypted ? "Encrypted." : "Decrypted"));
 
 file1.fname = "axl";
 file1.isEncrypted = false;
-console.log(file1.fname + " is " + (file1.isEncrypted ? "Encrypted." : "Decrypted"));
+console.log("INFO_DEBUG: " + file1.fname + " is " + (file1.isEncrypted ? "Encrypted." : "Decrypted"));
 /*** Sample Debug Files End ***/
 
 
-function decryptFile() {}
-function encryptFile() {}
+/*** This is the main functionality and shall be triggered by events from the site */
+encryptFile(file1);
+
+
